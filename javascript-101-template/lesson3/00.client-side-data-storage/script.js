4cf13cdfa601d45f37cd0d98ac5b6eab2a5317eb
maximelarrieu <maxime.larrieu@ynov.com>
2019-02-11 09:35:19 +0100

    new repo for new folders organization

---
+++ javascript-101-template/lesson3/00.client-side-data-storage/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Créer un cookie
+document.cookie = 'lang=fr'
+
+// Cookie et date d'expiration
+document.cookie = `lang=fr;expires=${new Date('2019-02-12')}`;
+
+// Import cookie dans cookie.js
+docCookies.setItem('id', 'valeur', new Date('2019-12-31'));
+
+//
+
+
+//
+
+
+//
