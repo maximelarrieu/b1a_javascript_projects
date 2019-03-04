2dfba0833ea8e76fec692e0b1f4e91fbb45685d5
maximelarrieu <maxime.larrieu@ynov.com>
2019-03-04 17:48:58 +0100

    02, 03, 04

--- javascript-101-template/lesson5/02.DOM-manipulation/script.js
+++ javascript-101-template/lesson5/02.DOM-manipulation/script.js
@@ -38,3 +38,47 @@ $('#js-search-form')[0].addEventListener('submit', () => {
 	`);
     }
 });
+
+// PART 2
+// Ajoutez la classe 'alert-primary' au <div>
+$('#js-alert').addClass('alert-primary');
+
+// Supprimez la classe 'alert-primary' du <div>. Supprimez 
+$('#js-alert').removeClass('alert-primary');
+
+// Ajoutez d'un coup les classes 'alert' et 'alert-warning' au <div>
+$('#js-alert').addClass('alert alert-warning');
+
+// Vérifiez si le <div> possède déjà la classe 'alert-warning'. Si oui, supprimez-la
+if ($('#js-alert').hasClass('alert-warning')) {
+    $('#js-alert').removeClass('alert-warning');
+}
+
+// En cliquant sur le bouton #js-btn, basculez la classe 'alert-success': premier clic ajoute la classe deuxième clic l'enlève, troisième l'ajoute de nouveau..
+$('#js-btn')[0].addEventListener('click', () => {
+    $('#js-alert').toggleClass('alert-success');
+});
+
+// Récupérez et logguez dans la console la couleur du fond du bouton.
+console.log($('#js-btn').css('background-color'));
+
+// Passez la couleur du fond du bouton en #71b8af
+$('#js-btn').css('background-color', '#71b8af');
+
+// Changez d'un coup la couleur du texte du bouton à #fff2d5 et le radius de la bordure à 3px
+$('#js-btn').css({
+    color: '#fff2d5',
+    'border-radius': '3px'
+});
+
+// Hide alert
+if ($('#js-danger-alert').hasClass('alert-danger')) {
+    $('div p').addClass('text-danger');
+    $('div button').removeClass('btn-success');
+}
+$('#js-danger-alert')[0].addEventListener('click', () => {
+    $('#js-danger-alert').css({
+	display: 'none'
+    });
+});
+
